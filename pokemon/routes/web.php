785bc1f789e56cb65b9c378cceea8c5ac7d6b1e6
Antoine Lambert <antoine.lambert@etu.imt-nord-europe.fr>
2022-12-04 04:39:28 +0100

    Jetstream install (with Livewire)

--- pokemon/routes/web.php
+++ pokemon/routes/web.php
@@ -18,3 +18,12 @@ Route::get('/', function () {
 });
 
 Route::get('/listePokemons', 'App\Http\Controllers\listePokemonsController@getListePokemons');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
