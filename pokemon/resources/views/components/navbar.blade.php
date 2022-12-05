50f6f040d878172cdaf87ae93868adee07b97826
El Mahdi Oukhamou <elmahdioukhamou@gmail.com>
2022-12-05 17:38:55 +0100

    Navbae is ready to be used. ( I can add smtg laterly )

--- pokemon/resources/views/components/navbar.blade.php
+++ pokemon/resources/views/components/navbar.blade.php
@@ -1,53 +1,57 @@
+<!--
+I tried to use bootsrap as much as I can (Stand on the Shoulders of Giants)
 
-<style>
-    nav {
+What I can add:
+  - Solve 'se connecter' Button
+  - Add icons
+  - Change navbar toggler to other one
+  - Add animations ( not important)
+
+-->
+    <nav class="navbar navbar-expand-lg bg-light rounded-4"
+    style="
       margin-left: 8%;
       margin-right: 8%;
       margin-top: 30px;
-      height: 100px;
-      border-radius: 20px;
-      
-    }
-</style>
-
-<div class="menu">
-    <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
+    "
+    >
+        <div class="container-fluid d-flex align-items-stretch">
           <img class="navbar-brand" src="images/Pokemon-Logo.png" width="150" height="93"/>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
+            
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent"
+          <div class="collapse navbar-collapse align-items-stretch align-self-stretch" id="navbarSupportedContent"
           style="padding-left: 5%"
-          >
+          > <!-- If we put add 'd-flex' class, we will have a reponsivity problem-->
             <ul class="navbar-nav me-auto mb-2 mb-lg-0" >
-              <li class="nav-item border-bottom border-5 border-success">
+              <li class="nav-item border-bottom border-5 border-success btn btn-outline-success p-2 ">
                 <a class="nav-link active" aria-current="page" href="/">
-                    <span class="h5">Accueil</span>
+                  <span class="h5 p-2">Accueil</span>
                 </a>
               </li>
-              <li class="nav-item border-bottom border-5 border-warning">
+              <li class="nav-item border-bottom border-5 border-warning btn btn-outline-warning p-2">
                 <a class="nav-link active" href="#">
                   <span class="h5">Bestaire</span> 
                 </a>
               </li>
               
-              <li class="nav-item border-bottom border-5 border-secondary">
+              <li class="nav-item border-bottom border-5 border-secondary btn btn-outline-secondary p-2">
                 <a class="nav-link active" href="#">
                   <span class="h5">Lancer un combat</span> 
                 </a>
               </li>
-              <li class="nav-item border-bottom border-5 border-info">
+              <li class="nav-item border-bottom border-5 border-info btn btn-outline-info p-2">
                 <a class="nav-link active" href="#" >
                   <span class="h5">Contactez nous</span> </a>
               </li>
     
-              <li class="nav-item rounded-4 rounded-left" 
+              <li class="nav-item border-bottom border-5 border-danger btn btn-outline-danger p-2" id="login-layout" 
               style="
-                background-color: red; 
                 
                 position:absolute;
                 right:0;
+              
                 " 
               >
                 <a 
@@ -61,4 +65,3 @@
           </div>
         </div>
       </nav>
-</div>
--- pokemon/resources/views/layouts/app.blade.php
+++ pokemon/resources/views/layouts/app.blade.php
@@ -2,47 +2,6 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-<<<<<<< Updated upstream
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Styles -->
-        @livewireStyles
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
-
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-
-        @stack('modals')
-
-        @livewireScripts
-    </body>
-=======
         
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -63,5 +22,5 @@
         <h1 style="color: azure">Hello World</h1>
     </body>
     @yield('script')
->>>>>>> Stashed changes
+
 </html>
