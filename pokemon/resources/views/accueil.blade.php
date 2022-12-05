0d5fc1712542fce17a6286a624d204fe6e4d374e
El Mahdi Oukhamou <elmahdioukhamou@gmail.com>
2022-12-05 23:26:41 +0100

    I start the the home page

--- pokemon/resources/views/accueil.blade.php
+++ pokemon/resources/views/accueil.blade.php
@@ -1,6 +1,28 @@
 @extends('layouts.app')
 
+@section('style')
+<style>
+#cc {
+      color: azure
+   }
+.card {
+      margin-left: 8%;
+      margin-right: 8%;
+}
+</style>
+  
+@endsection
 @section('template')
-   <h1 style="color: azure">La page d'Accueil</h1> 
+   <div class="card" style="">
+      
+       <video playsinline="" muted="" autoplay="" loop="" disableremoteplayback="">
+         <source src="https://storage.googleapis.com/pgoblog/seasons-mythical-wishes/Hero%20Trailer/PGO_S9_Launch_16x9_WebHeader_v1.mp4" type="video/mp4">
+         </video>
+      <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+   </div> 
    
 @endsection
