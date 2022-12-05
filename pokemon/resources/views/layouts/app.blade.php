a97e35a6c8e72fba639c94242f2eed60dc47ff67
El Mahdi Oukhamou <elmahdioukhamou@gmail.com>
2022-12-05 11:19:37 +0100

    Update app.blade.php

--- pokemon/resources/views/layouts/app.blade.php
+++ pokemon/resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+<<<<<<< Updated upstream
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -41,4 +42,26 @@
 
         @livewireScripts
     </body>
+=======
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <title>Pokemon Web Site</title>
+        @yield('style')
+    </head>
+    <style>
+        body {
+            background: rgba(12, 28, 62, 1);
+        }
+    </style>
+    <body >
+        @include('components.navbar')
+        @yield('template')
+        <h1 style="color: azure">Hello World</h1>
+    </body>
+    @yield('script')
+>>>>>>> Stashed changes
 </html>
