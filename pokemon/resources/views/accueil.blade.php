0f50de1a6e6955c6e8c6fb32a141c0590d644589
El Mahdi Oukhamou <elmahdioukhamou@gmail.com>
2022-12-05 11:18:00 +0100

    front-end update --- navbar

---
+++ pokemon/resources/views/accueil.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('template')
+   <h1 style="color: azure">La page d'Accueil</h1> 
+   
+@endsection
---
+++ pokemon/resources/views/components/navbar.blade.php
@@ -0,0 +1,64 @@
+
+<style>
+    nav {
+      margin-left: 8%;
+      margin-right: 8%;
+      margin-top: 30px;
+      height: 100px;
+      border-radius: 20px;
+      
+    }
+</style>
+
+<div class="menu">
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <img class="navbar-brand" src="images/Pokemon-Logo.png" width="150" height="93"/>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent"
+          style="padding-left: 5%"
+          >
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0" >
+              <li class="nav-item border-bottom border-5 border-success">
+                <a class="nav-link active" aria-current="page" href="/">
+                    <span class="h5">Accueil</span>
+                </a>
+              </li>
+              <li class="nav-item border-bottom border-5 border-warning">
+                <a class="nav-link active" href="#">
+                  <span class="h5">Bestaire</span> 
+                </a>
+              </li>
+              
+              <li class="nav-item border-bottom border-5 border-secondary">
+                <a class="nav-link active" href="#">
+                  <span class="h5">Lancer un combat</span> 
+                </a>
+              </li>
+              <li class="nav-item border-bottom border-5 border-info">
+                <a class="nav-link active" href="#" >
+                  <span class="h5">Contactez nous</span> </a>
+              </li>
+    
+              <li class="nav-item rounded-4 rounded-left" 
+              style="
+                background-color: red; 
+                
+                position:absolute;
+                right:0;
+                " 
+              >
+                <a 
+                class="nav-link active" 
+                href="#" 
+                >
+                <span class="h5">Se connecter</span> </a>
+              </li>
+            </ul>
+            
+          </div>
+        </div>
+      </nav>
+</div>
