0e88c5b6853d082e14fb877ab725eea5e1febeaa
Antoine Lambert <antoine.lambert@etu.imt-nord-europe.fr>
2022-12-07 09:59:37 +0100

    update frontend

--- pokemon/resources/views/layouts/app.blade.php
+++ pokemon/resources/views/layouts/app.blade.php
@@ -11,34 +11,19 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        @livewireStyles
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
+        @yield('style')
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- Bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
-
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-
-        @stack('modals')
-
-        @livewireScripts
+    <body>
+        @include('navbar')
+        @yield('content')
+        <h1>TEST</h1>
+        @include('footer')
     </body>
+    @yield('script')
 </html>
--- pokemon/routes/web.php
+++ pokemon/routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\homeController@getHome');
 
 Route::get('/listePokemons', 'App\Http\Controllers\listePokemonsController@getListePokemons');
 
