f5749805a0810d57a4e5f8b2a7473f60feb1ecc5
Antoine Lambert <antoine.lambert@etu.imt-nord-europe.fr>
2022-12-07 10:18:10 +0100

    frontend update (name & icon)

--- pokemon/resources/views/layouts/app.blade.php
+++ pokemon/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Pokemon App') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -15,6 +15,8 @@
         <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
         <link rel="stylesheet" href="css/style.css">
         @yield('style')
+
+        <link rel="icon" type="image/png" sizes="16x16" href="assets/img/logo/icon.png">
 
         <!-- Bootstrap -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
